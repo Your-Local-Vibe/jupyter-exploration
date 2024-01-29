45e152486844c6024a4a507a20f1af6c5f6da4c2
Your-Local-Vibe <144163946+Your-Local-Vibe@users.noreply.github.com>
2024-01-29 14:41:35 -0600

    Add files via upload

---
+++ My_First_Notebook.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# The purpose of cells is to be used for different purposes within the code. For example this cell "markdown" is used for statements or to divide sections and label them. This is not to be confused with # statements which their purpose is for mini notes, and to explain certain parts of codes like their purposes. Code cells on the other hand are used for the purpose of coding. Well explained by the name it is where coders would impliment their code and execute it.
+
+# In[1]:
+
+
+print("Welcome to the code paradise")
+
+
+# This is my second markdown cell on this jupyter notebook.
+
+# In[ ]:
+
+
+
+
